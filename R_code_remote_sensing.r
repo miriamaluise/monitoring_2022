8f8206f63a422f086788f91e12799f5bd41c851b
miriamaluise <92572884+miriamaluise@users.noreply.github.com>
2021-10-29 11:01:17 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -7,6 +7,19 @@
 #https://cran.r-project.org/web/packages/raster/index.html
 install.packages("raster")
 
-library(raster)
 
+library(raster)
+setwd("C:/lab/")
+#we are going to import satellite data
+l2011 <- brick("p224r63_2011.grd")
+#objects cannot be numbers
+
+plot(l2011)
+#B1 is the refecltance in the blue bend
+#B2 is the reflectance in the green bend
+#B3 is the reflectance in the red bend
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011,col=cl)
+plotRGB(l2011, r=3, g=2, b=1,stretch="Lin")
 

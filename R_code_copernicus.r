0be563a8818d21d498de79bd597ec14bb2198cf0
miriamaluise <92572884+miriamaluise@users.noreply.github.com>
2021-12-16 12:15:21 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -6,3 +6,47 @@ library(raster)
 snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
 snow20211214
 plot(snow20211214)
+
+#___________________________________day 2
+library(ncdf4)
+setwd("C:/lab/copernicus/")
+library(raster)
+snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
+snow20211214
+plot(snow20211214)
+
+cl <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(snow20211214, col=cl)
+
+install.packages("viridis")
+library(viridis)
+library(RStoolbox)
+library(ggplot2)
+#ggplot function
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent))
+
+#ggplot function with viridis
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
+scale_fill_viridis()
+
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
+scale_fill_viridis(option="inferno")
+
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
+scale_fill_viridis(option="cividis") +
+ggtitle("cividis palette")
+
+
+
+
+
+
+
+
+
+
+
